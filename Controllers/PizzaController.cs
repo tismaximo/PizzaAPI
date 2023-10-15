96ba037691d29f354d79c1930970b01074ab5cad
Maximo Javier Tiscornia <superxgoku23@gmail.com>
2023-10-15 15:58:14 -0300

    Initial commit

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ContosoPizza.Models;
+using WebApplication1.Services;
+
+namespace ContosoPizza.Controller;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+    public PizzaController()
+    {
+    }
+
+    //HTTP GET request
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
+
+    //HTTP get request by id
+    [HttpGet("{Id}")]
+    public ActionResult<Pizza?> Get(int id) => PizzaService.Get(id);
+
+    //HTTP post request
+    [HttpPost]
+    public IActionResult Create(Pizza pizza)
+    {
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Get), new { Id = pizza.Id },  pizza);
+    }
+
+    //HTTP PUT request
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Pizza pizza)
+    {
+        if (id != pizza.Id)
+            return BadRequest();
+        var updatePizza = PizzaService.Get(id);
+        if (updatePizza is null)
+            return NotFound();
+        PizzaService.Update(pizza);
+        return NoContent();
+    }
+
+    //HTTP DELETE request
+    [HttpDelete("{id}")]
+    public IActionResult Remove(int id)
+    {
+        var pizza = PizzaService.Get(id); 
+        if (pizza is null) 
+            return NotFound();
+        PizzaService.Remove(id);
+        return NoContent();
+    }
+}
---
+++ Services/PizzaService.cs
@@ -0,0 +1,97 @@
+﻿using ContosoPizza.Models;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Text;
+using System.Text.Json;
+
+namespace WebApplication1.Services;
+
+public static class PizzaService
+{
+    static private string fileName = "data.json";
+    static List<Pizza> pizzas;
+    static int nextId;
+    static void findNextId()
+    {
+        if (!File.Exists(fileName))
+        {
+            File.Create(fileName).Close();
+            nextId = 1;
+        }
+        else
+        {
+            nextId = File.ReadAllLines(fileName).Length + 1;
+        }
+    }
+    static PizzaService()
+    {
+        //Initialize pizzas list based off the data in data.json
+        findNextId();
+        pizzas = new List<Pizza>();
+        string[] lines;
+        lines = File.ReadAllLines(fileName);
+        Pizza? pizza = new Pizza();
+        foreach (string line in lines)
+        {
+            pizza = JsonSerializer.Deserialize<Pizza>(line);
+            if (pizza != null) pizzas.Add(pizza);
+        }
+    }
+
+    ///<summary>
+    ///Add a pizza to the list and to the json file.
+    ///</summary>
+    public static void Add(Pizza pizza)
+    {
+
+        pizza.Id = nextId++;
+        pizzas.Add(pizza);
+        StreamWriter streamWriter = new StreamWriter(fileName, true);
+        streamWriter.WriteLine(JsonSerializer.Serialize(pizza));
+        streamWriter.Close();
+    }
+
+    ///<summary>
+    ///Returns all the pizzas.
+    ///</summary>
+    public static List<Pizza> GetAll() => pizzas;
+    ///<summary>
+    ///Returns a pizza from an ID. Returns null if no matching ID is found.
+    ///</summary>
+    public static Pizza? Get(int id) => pizzas.FirstOrDefault(p => p.Id == id);
+
+    ///<summary>
+    ///Removes a pizza from the list and from the json file.
+    ///</summary>
+    public static void Remove(int id)
+    {
+        var pizza = Get(id);
+        int index = id - 1;
+        if (pizza is not null)
+        {
+            pizzas.Remove(pizza);
+            List<string> lines = new List<string>();
+            foreach (string line in File.ReadAllLines(fileName))
+            {
+                if (JsonSerializer.Deserialize<Pizza>(line)?.Id != id)
+                    lines.Add(line);
+            }
+            File.WriteAllLines(fileName, lines);
+        }
+    }
+
+    ///<summary>
+    ///Updates a Pizza that matches the parameter pizza's ID. Updates the json file.
+    ///</summary>
+    public static void Update(Pizza newPizza)
+    {
+        var index = pizzas.FindIndex(p => p.Id == newPizza.Id);
+        if (index > -1)
+        {
+            pizzas[index] = newPizza;
+            string[] lines = File.ReadAllLines(fileName);
+            lines[index] = JsonSerializer.Serialize(newPizza);
+            File.WriteAllLines(fileName, lines);
+        }
+    }
+}
